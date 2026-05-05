6bf31091a56f1281893d07d1580e62bdff2c802d
Gabriel Ramirez Salas <gabriel.ramirezita@gmail.com>
2026-05-05 12:09:21 +0200

    Add initial implementation of bikes service with Maven setup and configuration

---
+++ bikes-service/src/test/java/com/eleventh/workshop/bikes_service/BikesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleventh.workshop.bikes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
