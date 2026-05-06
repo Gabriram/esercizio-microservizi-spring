a1ebb6e7cb69b42e47ce88f455d207df53e582a5
Gabriram <gabriel.ramirezita@gmail.com>
2026-05-06 21:24:37 +0200

    fix: Update GarageService to correctly add vehicles to the garage

--- garage/src/main/java/services/GarageService.java
+++ garage/src/main/java/services/GarageService.java
@@ -4,22 +4,14 @@ import org.springframework.stereotype.Service;
 import entities.Vehicle;
 import repository.GarageRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.client.HttpClientErrorException;
-import dtos.GarageModifyDataDto;
-import dtos.VehicleModifyDataDto;
-import dtos.GarageResponseDto;
-import dtos.VehicleResponseDto;
 import entities.Garage;
-import dtos.*;
 import org.springframework.http.HttpStatus;
 import repository.VehicleRepository;
 
 @Service
 public class GarageService {
 
-    private final VehicleRepository vehicleRepository;
     private final VehicleValidator vehicleValidator;
     private final GarageRepository garageRepository;
 
@@ -29,23 +21,22 @@ public class GarageService {
     @Value("${service.cars.url}")
     private String carsUrl;
 
-    GarageService(VehicleValidator vehicleValidator, GarageRepository garageRepository, VehicleRepository vehicleRepository) {
+    GarageService(VehicleValidator vehicleValidator, GarageRepository garageRepository,
+            VehicleRepository vehicleRepository) {
         this.vehicleValidator = vehicleValidator;
         this.garageRepository = garageRepository;
-        this.vehicleRepository = vehicleRepository;
     }
 
- public Vehicle addVehicle(Long garageId, Vehicle v) {
-    if (!vehicleValidator.exists(v.getVehicleType(), v.getExternalId())) {
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-            "Vehicle " + v.getVehicleType() + ":" + v.getExternalId() + " does not exist");
-    }
-    Garage garage = garageRepository.findById(garageId)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    v.setGarage(garage);
-        garage.addVehicle(v);
-    garageRepository.save(garage);
-    return v;
-}   
+    public Vehicle addVehicle(Long garageId, Vehicle v) {
+        if (!vehicleValidator.exists(v.getVehicleType(), v.getExternalId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Vehicle " + v.getVehicleType() + ":" + v.getExternalId() + " does not exist");
+        }
+        Garage garage = garageRepository.findById(garageId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        v.setGarage(garage);
+        garage.getVehicles().add(v);
+        garageRepository.save(garage);
+        return v;
     }
 }
