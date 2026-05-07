60337661fe05dd47c96a06196d302c9a68ee0bfe
Gabriel Ramirez Salas <gabriel.ramirezita@gmail.com>
2026-05-07 13:11:40 +0200

    feat: Add constructors to GarageModifyDataDto and VehicleModifyDataDto for improved object initialization

--- garage/src/main/java/com/corso/garage/dtos/GarageModifyDataDto.java
+++ garage/src/main/java/com/corso/garage/dtos/GarageModifyDataDto.java
@@ -1,11 +1,16 @@
 package com.corso.garage.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GarageModifyDataDto {
 
     private String ownerName;
--- garage/src/main/java/com/corso/garage/dtos/VehicleModifyDataDto.java
+++ garage/src/main/java/com/corso/garage/dtos/VehicleModifyDataDto.java
@@ -1,10 +1,14 @@
 package com.corso.garage.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleModifyDataDto {
     private String vehicleType;
     private Long externalId;
